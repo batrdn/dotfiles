1459469cf97686682cf9712dec60b357b6bd07ab
batrdn <batrdn220@gmail.com>
2026-01-18 17:30:36 +0100

    add cursor settings + vscode-neovim config

--- nvim/lua/batrdn/init.lua
+++ nvim/lua/batrdn/init.lua
@@ -1,6 +1,8 @@
 require('batrdn.keymaps')
 if vim.g.vscode then
-    -- VSCode extension
+    vim.keymap.set('n', 'gr', '<Cmd>call VSCodeNotify("editor.action.goToReferences")<CR>', { desc = 'Find references' })
+    vim.keymap.set('n', '<leader>rn', '<Cmd>call VSCodeNotify("editor.action.rename")<CR>', { desc = '[R]e[n]ame' })
+    vim.keymap.set('n', 'K', '<Cmd>call VSCodeNotify("editor.action.peekDefinition")<CR>', { desc = 'Peek definition' })
 else
     require('batrdn.autocmds')
     require('batrdn.lazy')
